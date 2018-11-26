72cc793ae9b21cc14df202f39b08f98ffb081e64
nontachaiwebdev <sutininter@gmail.com>
2018-11-26 17:17:02 +0700

    refactor(pointCalc): Add setpoint score to array

--- tennis/score.go
+++ tennis/score.go
@@ -12,34 +12,22 @@ func NewScoreBoard() ScoreBoard {
 }
 
 func (s *ScoreBoard) AGetPoint() string {
-	var scoreText string
-	if s.aPoint == 3 {
-		bPointText := pointCalc(s.bPoint)
-		scoreText = fmt.Sprintf("setpoint - %s", bPointText)
-	} else {
-		s.aPoint++
-		aPointText := pointCalc(s.aPoint)
-		bPointText := pointCalc(s.bPoint)
-		scoreText = fmt.Sprintf("%s - %s", aPointText, bPointText)
-	}
+	s.aPoint++
+	aPointText := pointCalc(s.aPoint)
+	bPointText := pointCalc(s.bPoint)
+	scoreText := fmt.Sprintf("%s - %s", aPointText, bPointText)
 	return scoreText
 }
 
 func (s *ScoreBoard) BGetPoint() string {
-	var scoreText string
-	if s.bPoint == 3 {
-		aPointText := pointCalc(s.aPoint)
-		scoreText = fmt.Sprintf("%s - setpoint", aPointText)
-	} else {
-		s.bPoint++
-		aPointText := pointCalc(s.aPoint)
-		bPointText := pointCalc(s.bPoint)
-		scoreText = fmt.Sprintf("%s - %s", aPointText, bPointText)
-	}
+	s.bPoint++
+	aPointText := pointCalc(s.aPoint)
+	bPointText := pointCalc(s.bPoint)
+	scoreText := fmt.Sprintf("%s - %s", aPointText, bPointText)
 	return scoreText
 }
 
 func pointCalc(point int) string {
-	score := [4]string{"0", "15", "30", "40"}
+	score := [5]string{"0", "15", "30", "40", "setpoint"}
 	return score[point]
 }
